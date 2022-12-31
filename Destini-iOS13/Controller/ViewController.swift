4378cb235e622271df04634272949488ca2819ee
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2022-12-31 15:12:10 -0800

    made changes to the design

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    
+    var story = Story("You see a fork in the road", "Take a left", "Take a right")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = story.stories[0][0]
+        choice1Button.setTitle(story.stories[0][1], for: .normal)
+        choice2Button.setTitle(story.stories[0][2], for: .normal)
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton)
+    {
+        if sender.title(for: .normal) == "Choice 1"
+        {
+            storyLabel.text = story.stories[1][0]
+        }
+        else
+        {
+            storyLabel.text = story.stories[2][0]
+        }
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+struct Story
+{
+    var title : String
+    var choice1 : String
+    var choice2 : String
+    
+    let stories = [
+        ["You see a fork in the road.", "Take a left.", "Take a right."],
+        ["You see a tiger.", "Take a left.", "Take a right."],
+        ["You find a treasure chest.", "Open it.", "Check for traps."]]
+    
+    init(_ title : String, _ choice1 : String, _ choice2 : String)
+    {
+        self.title = title
+        self.choice2 = choice2
+        self.choice1 = choice1
+    }
+}
